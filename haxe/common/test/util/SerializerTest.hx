b695507e096c8ce043aad6c2ba719b52e544aee6
Skyler Parr <skylerparr@gmail.com>
2013-09-16 11:26:28 -0700

    removed com.thoughtorigin from package

---
+++ haxe/common/test/util/SerializerTest.hx
@@ -0,0 +1,252 @@
+package util;
+
+import vo.ValueObject;
+import massive.munit.util.Timer;
+import massive.munit.Assert;
+import massive.munit.async.AsyncFactory;
+
+class SerializerTest {
+
+
+    public function new() {
+
+    }
+
+    @Before
+    public function setup():Void {
+    }
+
+    @After
+    public function tearDown():Void {
+    }
+
+    @Test
+    public function shouldSerializeStrings(): Void {
+        var s: Sample1 = new Sample1();
+        s.name = "foo";
+        var o: Dynamic = Serializer.serialize(s);
+        Assert.areEqual("foo", o.name);
+    }
+
+    @Test
+    public function shouldSerializeInts(): Void {
+        var s: Sample2 = new Sample2();
+        s.count = 100;
+        var o: Dynamic = Serializer.serialize(s);
+        Assert.areEqual(100, o.count);
+    }
+
+    @Test
+    public function shouldSerializeNestValueObjects(): Void {
+        var s1: Sample1 = new Sample1();
+        s1.name = "foo";
+        var s2: Sample2 = new Sample2();
+        s2.count = 100;
+        var s4: Sample4 = new Sample4();
+        s4.s1 = s1;
+        var s3: Sample3 = new Sample3();
+        s3.id = "39";
+        s3.s4 = s4;
+        s3.s2 = s2;
+        var o: Dynamic = Serializer.serialize(s3);
+        Assert.areEqual("39", o.id);
+        Assert.areEqual("foo", o.s4.s1.name);
+        Assert.areEqual(100, o.s2.count);
+    }
+
+    @Test
+    public function shouldNotIncludeAValueIfItIsNull(): Void {
+        var s2: Sample2 = new Sample2();
+        s2.count = 100;
+        var s3: Sample3 = new Sample3();
+        s3.id = "39";
+        s3.s4 = null;
+        s3.s2 = s2;
+        var o: Dynamic = Serializer.serialize(s3);
+        Assert.areEqual("39", o.id);
+        Assert.areEqual(null, o.s4);
+    }
+
+    @Test
+    public function shouldNotIncludeAValueIfItIsWriteOnly(): Void {
+        var s1: Sample1 = new Sample1();
+        var s4: Sample4 = new Sample4();
+        s4.s1 = s1;
+        var o: Dynamic = Serializer.serialize(s4);
+        Assert.areEqual(null, o.value);
+    }
+
+    @Test
+    public function shouldDeserializeToTheRightType(): Void {
+        var s: Sample1 = new Sample1();
+        s.name = "foo";
+        var o: Dynamic = Serializer.serialize(s);
+        var r: Sample1 = Serializer.deserialize(o);
+        Assert.areEqual("foo", r.name);
+    }
+
+    @Test
+    public function shouldDeserializeNestedValueObjects(): Void {
+        var s1: Sample1 = new Sample1();
+        s1.name = "foo";
+        var s2: Sample2 = new Sample2();
+        s2.count = 100;
+        var s4: Sample4 = new Sample4();
+        s4.s1 = s1;
+        var s3: Sample3 = new Sample3();
+        s3.id = "39";
+        s3.s4 = s4;
+        s3.s2 = s2;
+        var o: Dynamic = Serializer.serialize(s3);
+        var r: Sample3 = Serializer.deserialize(o);
+        Assert.areEqual("39", r.id);
+        Assert.areEqual("foo", r.s4.s1.name);
+        Assert.areEqual(100, r.s2.count);
+    }
+
+    @Test
+    public function shouldNotFailIfValueIsReadOnly(): Void {
+        var s1: Sample1 = new Sample1();
+        var s4: Sample4 = new Sample4();
+        s4.s1 = s1;
+        var o: Dynamic = Serializer.serialize(s4);
+        var r: Sample4 = Serializer.deserialize(o);
+        Assert.areEqual(null, r.value2);
+    }
+
+    @Test
+    public function shouldSerializeArrays(): Void {
+        var s5: Sample5 = new Sample5();
+        s5.array.push("1");
+        s5.array.push("2");
+        s5.array.push("3");
+        var o: Dynamic = Serializer.serialize(s5);
+        Assert.areEqual("1", o.array[0]);
+        Assert.areEqual("2", o.array[1]);
+        Assert.areEqual("3", o.array[2]);
+    }
+
+    @Test
+    public function shouldSerializeArraysWithNestValueObjects(): Void {
+        var s5: Sample5 = new Sample5();
+        var s1: Sample1 = new Sample1();
+        s1.name = "foo1";
+        s5.array.push(s1);
+        var s1: Sample1 = new Sample1();
+        s1.name = "foo2";
+        s5.array.push(s1);
+        var s1: Sample1 = new Sample1();
+        s1.name = "foo3";
+        s5.array.push(s1);
+        var o: Dynamic = Serializer.serialize(s5);
+        Assert.areEqual("foo1", o.array[0].name);
+        Assert.areEqual("foo2", o.array[1].name);
+        Assert.areEqual("foo3", o.array[2].name);
+    }
+
+    @Test
+    public function shouldDeserializeArrays(): Void {
+        var s5: Sample5 = new Sample5();
+        var s1: Sample1 = new Sample1();
+        s1.name = "foo1";
+        s5.array.push(s1);
+        var s1: Sample1 = new Sample1();
+        s1.name = "foo2";
+        s5.array.push(s1);
+        var s1: Sample1 = new Sample1();
+        s1.name = "foo3";
+        s5.array.push(s1);
+        var o: Dynamic = Serializer.serialize(s5);
+        var s: Sample5 = Serializer.deserialize(o);
+        Assert.areEqual("foo1", s.array[0].name);
+        Assert.areEqual("foo2", s.array[1].name);
+        Assert.areEqual("foo3", s.array[2].name);
+    }
+
+    @Test
+    public function shouldNotSerializeNonValueObjects(): Void {
+        var s5: Sample5 = new Sample5();
+        var s6: Sample6 = new Sample6();
+        s5.s6 = s6;
+        var o: Dynamic = Serializer.serialize(s5);
+        Assert.areEqual(null, o.s6);
+    }
+
+    @Test
+    public function shouldBeAbleToPutNullsIntoArrays(): Void {
+        var s5: Sample5 = new Sample5();
+        s5.array.push(null);
+        s5.array.push(null);
+        s5.array.push(null);
+        var o: Dynamic = Serializer.serialize(s5);
+        Assert.areEqual(null, o.array[0]);
+        Assert.areEqual(null, o.array[1]);
+        Assert.areEqual(null, o.array[2]);
+    }
+
+    @Test
+    public function shouldBeAbleTodeserializeAndPutNullsIntoArrays(): Void {
+        var s5: Sample5 = new Sample5();
+        s5.array.push(null);
+        s5.array.push(null);
+        s5.array.push(null);
+        var o: Dynamic = Serializer.serialize(s5);
+        var v: Sample5 = Serializer.deserialize(o);
+        Assert.areEqual(null, v.array[0]);
+        Assert.areEqual(null, v.array[1]);
+        Assert.areEqual(null, v.array[2]);
+    }
+}
+
+class Sample1 implements ValueObject {
+    public var name: String;
+
+    public function new() {
+
+    }
+}
+
+class Sample2 implements ValueObject {
+    public var count: Int;
+
+    public function new() {
+
+    }
+}
+
+class Sample3 implements ValueObject {
+    public var id: String;
+    public var s4: Sample4;
+    public var s2: Sample2;
+    public function new() {
+
+    }
+}
+
+class Sample4 implements ValueObject {
+    public var s1: Sample1;
+    public var value(null, set): String;
+    public var value2(get, null): String;
+
+    public function new() {}
+    private function set_value(v: String): String {
+        value = v;
+        return v;
+    }
+    private function get_value2(): String {
+        return value2;
+    }
+}
+
+class Sample5 implements ValueObject {
+    public var array: Array<Dynamic>;
+    public var s6: Sample6;
+    public function new() {
+        array = new Array();
+    }
+}
+
+class Sample6 {
+    public var foo: String;
+    public function new() {}
+}
