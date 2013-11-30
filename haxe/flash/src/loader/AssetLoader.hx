b747c464f6cf7a2854b1283869a87e61ed788c4d
Skyler Parr <skylerparr@gmail.com>
2013-11-30 13:24:57 -0800

    added load text to interface

--- haxe/flash/src/loader/AssetLoader.hx
+++ haxe/flash/src/loader/AssetLoader.hx
@@ -2,4 +2,5 @@ package loader;
 import flash.display.BitmapData;
 interface AssetLoader {
     function loadImage(imageName: String, onComplete: BitmapData->Void, onFail: String->Void = null): Void;
+    function loadText(name: String, onComplete: String->Void, onFail: String->Void = null): Void;
 }
