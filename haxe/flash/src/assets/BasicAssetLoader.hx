1e8820d921185281c7ccdb2cd2c1ec9349a20fc9
Skyler Parr <skylerparr@gmail.com>
2014-03-27 22:33:34 -0700

    added feature so that asset loader doesn't load the same image more than one at a time

--- haxe/flash/src/assets/BasicAssetLoader.hx
+++ haxe/flash/src/assets/BasicAssetLoader.hx
@@ -12,10 +12,12 @@ class BasicAssetLoader implements AssetLoader {
     private var _req: URLRequest;
     private var _loadQueue: Array<LoadDef>;
     private var _isLoading: Bool;
+    private var _loadMap: Map<String, Array<LoadDef>>;
 
     public function new() {
         _req = new URLRequest();
         _loadQueue = [];
+        _loadMap = new Map();
     }
 
     public function loadImage(imageName:String, onComplete:BitmapData -> Void, onFail:String -> Void = null):Void {
@@ -28,27 +30,38 @@ class BasicAssetLoader implements AssetLoader {
         load(name, onComplete, onFail, true);
     }
 
-    private inline function load(name:String, onComplete:Dynamic -> Void, onFail:String -> Void = null, loadText: Bool = false): Void {
-        _loadQueue.push({imageName: name, onComplete: onComplete, onFail: onFail, loadText: loadText});
+    public function load(name:String, onComplete:Dynamic -> Void, onFail:String -> Void = null, loadText: Bool = false): Void {
+        var loadDef: LoadDef = {imageName: name, onComplete: onComplete, onFail: onFail, loadText: loadText};
+        if(_loadMap.exists(name)) {
+            var collection: Array<LoadDef> = _loadMap.get(name);
+            collection.push(loadDef);
+            return;
+        }
+        _loadQueue.push(loadDef);
+        _loadMap.set(name, [loadDef]);
         if(!_isLoading) {
             _isLoading = true;
-            doLoad(loadText);
+            doLoad();
         }
     }
 
-    private function doLoad(loadText: Bool = false): Void {
+    private function doLoad(): Void {
         var loadDef: LoadDef = _loadQueue.shift();
         if(loadDef != null) {
             var loader: LoaderProxy = getLoader(loadDef.loadText);
             loader.addEventListener(Event.COMPLETE, function(e: Event): Void {
-                loadDef.onComplete(loader.content);
-                doLoad(loadDef.loadText);
+                var loadDefs: Array<LoadDef> = _loadMap.get(loadDef.imageName);
+                for(def in loadDefs) {
+                    def.onComplete(loader.content);
+                }
+                _loadMap.remove(loadDef.imageName);
+                doLoad();
             });
             loader.addEventListener(IOErrorEvent.IO_ERROR, function(e: IOErrorEvent): Void {
                 if(loadDef.onFail != null) {
                     loadDef.onFail(e.text);
                 }
-                doLoad(loadDef.loadText);
+                doLoad();
             });
             _req.url = getUrl(loadDef);
             loader.load(_req);
