5d6cd1588ec0d8a78ffaad4b6faff06427182870
Skyler Parr <skylerparr@gmail.com>
2014-04-28 11:53:13 -0700

    updated error codes

--- haxe/common/src/constants/AuthenticationErrorCodes.hx
+++ haxe/common/src/constants/AuthenticationErrorCodes.hx
@@ -3,6 +3,8 @@ class AuthenticationErrorCodes {
 
     public static inline var DEVICE_ID_NOT_FOUND: Int = 1;
     public static inline var SERVER_ERROR: Int = 2;
+    public static inline var USER_NOT_FOUND: Int = 3;
+    public static inline var BAD_PASSWORD: Int = 4;
 
     public function new() {
     }
