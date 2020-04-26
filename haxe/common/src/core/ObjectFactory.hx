fe55a76a59791d9bee36571cc29741a29083cb8c
Skyler Parr <skylerparr@gmail.com>
2020-04-26 10:59:50 -0400

    added a dispose instance function

--- haxe/common/src/core/ObjectFactory.hx
+++ haxe/common/src/core/ObjectFactory.hx
@@ -2,34 +2,38 @@ package core;
 import minject.Injector;
 class ObjectFactory implements ObjectCreator {
 
-    public static var injector(default, default): Injector;
+  public static var injector(default, default): Injector;
 
-    public function new() {
-    }
+  public function new() {
+  }
 
-    public static function createObject(clazz: Class<Dynamic>, ?constructorArgs: Array<Dynamic>): Dynamic {
-        if(constructorArgs == null) {
-            constructorArgs = [];
-        }
-        var retVal: Dynamic = null;
-        try {
-             retVal = injector.getInstance(clazz);
-        } catch(e: Dynamic) {
-            retVal = Type.createInstance(clazz, constructorArgs);
-            injector.injectInto(retVal);
-        }
-        if(Std.is(retVal, BaseObject)) {
-            retVal.init();
-        }
-        return retVal;
+  public static function createObject(clazz: Class<Dynamic>, ?constructorArgs: Array<Dynamic>): Dynamic {
+    if(constructorArgs == null) {
+      constructorArgs = [];
     }
+    var retVal: Dynamic = null;
+    try {
+      retVal = injector.getInstance(clazz);
+    } catch(e: Dynamic) {
+      retVal = Type.createInstance(clazz, constructorArgs);
+      injector.injectInto(retVal);
+    }
+    if(Std.is(retVal, BaseObject)) {
+      retVal.init();
+    }
+    return retVal;
+  }
 
-    public static function inject(object: Dynamic): Void {
-        injector.injectInto(object);
-    }
+  public static function inject(object: Dynamic): Void {
+    injector.injectInto(object);
+  }
 
-    public function createInstance(clazz: Class<Dynamic>, ?constructorArgs: Array<Dynamic>): Dynamic {
-        return ObjectFactory.createObject(clazz, constructorArgs);
-    }
+  public function createInstance(clazz: Class<Dynamic>, ?constructorArgs: Array<Dynamic>): Dynamic {
+    return ObjectFactory.createObject(clazz, constructorArgs);
+  }
+
+  public function disposeInstance(object: BaseObject): Void {
+    object.dispose();
+  }
 
 }
